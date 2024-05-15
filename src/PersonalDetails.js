5a24209fa1f0103ce3e2c81cc8d6a2f1b7660888
SubhashNalam <subhashnalam@gmail.com>
2024-05-15 17:55:11 +0530

    Eleventh-Commit

--- src/PersonalDetails.js
+++ src/PersonalDetails.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Resume from "./Subhash_Nalam_CV.pdf";
 
 function PersonalDetails() {
   return (
     <div>
       <h2>Personal Details</h2>
-      <p className="container">
+      <div className="container">
         <ul>
           <li>Date of Birth: 09-07-1998</li>
           <li>Gender: Male</li>
@@ -13,7 +14,16 @@ function PersonalDetails() {
           <li>Languages Known: Telugu, English</li>
           <li>Nationality: Indian</li>
         </ul>
-      </p>
+      </div>
+      <br></br>
+      <a
+        href={Resume}
+        download="Subhash_Nalam_CV"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <button className="btn">Download CV</button>
+      </a>
     </div>
   );
 }
